b6129fe3a8bdb2b42047dfa13c0b5acae6ca48d8
Adrian Anaemeje <anaemeje@mit.edu>
2023-06-06 01:06:38 -0400

    Implemented WFC alg with a sudoku board.

---
+++ src/wfc/sudoku_wfc/sudoku_board.py
@@ -0,0 +1,196 @@
+"""This module implements a sudoku board to be used with the WFC algorithm."""
+
+import random
+from typing import List, Dict, Self, Tuple, Set
+from ..wavefunction import Wavefunction
+from .sudoku_space import SudokuSpace
+
+class SudokuBoard(Wavefunction[SudokuSpace]):
+    """
+    An immutable Sudoku board to be used in the WFC algorthim.
+
+    Attributes:
+        wavefunction (List[List[SudokuSpace]]): The state of the wavefunction
+    """
+
+    def __init__(
+        self,
+        board_size: int,
+        initial_values: Dict[int, List[Tuple[int, int]]] | None = None,
+    ):
+        """
+        Initialize new Sudoku board.
+
+        Args:
+            board_size (int): The size of the board. Must be a perfect square.
+            initial_values (Dict[int, List[Tuple[int, int]]] | None): A list of values to add to the board. Defaults to None.
+
+        Raises:
+            ValueError: board_size is not a perfect sqaure.
+            ValueError: Provided board is unsolvable.
+        """
+        if board_size**0.5 != int(board_size**0.5):
+            raise ValueError("board_size needs to be a perfect square")
+        self.board_size = board_size
+        self._wavefunction: List[List[SudokuSpace]] = [
+            [SudokuSpace(x, y, max_value=board_size) for x in range(board_size)]
+            for y in range(board_size)
+        ]
+
+        count = 0
+        if initial_values:
+            value: int
+            coords: List[Tuple[int, int]]
+            for value, coords in initial_values.items():
+                for coord in coords:
+                    count += 1
+                    space = self._wavefunction[coord[1]][coord[0]]
+                    if value not in space.possible_values:
+                        raise ValueError("Provided board is unsolvable.")
+                    space.validate_value(value)
+                    space.value = value
+                    space.possible_values = {value}
+                    space.neighbors.remove(value)
+                    propegation_result = self.propegate(space)
+
+                    if propegation_result:
+                        self._wavefunction = propegation_result._wavefunction
+                    else:
+                        raise ValueError("Provided board is unsolvable.")
+
+    @property
+    def wavefunction(self) -> List[List[SudokuSpace]]:
+        """The current state of the sudoku wavefunction."""
+        return self._wavefunction
+
+    def get_min_entropy_tile(self) -> SudokuSpace | None:
+        """
+        Determine the space with the fewest options (> 1). Selects randomly if there are multiple.
+
+        Returns:
+            The space on the sudoku board with the lowest number of options.
+        """
+        min_value = float("inf")
+        min_spaces: List[SudokuSpace] = []
+
+        for row in self.wavefunction:
+            for space in row:
+                if space.value is not None:
+                    continue
+
+                if len(space.possible_values) < min_value:
+                    min_spaces = [space]
+                    min_value = len(space.possible_values)
+                elif len(space.possible_values) == min_value:
+                    min_spaces.append(space)
+
+        return random.choice(min_spaces) if min_spaces else None
+
+    def is_collapsed(self) -> bool:
+        """
+        Check if the sudoku board is fully solved.
+
+        Returns:
+            True if the sudoku board is fully solved. False otherwise.
+        """
+        return all(
+            all(space.value is not None for space in row) for row in self.wavefunction
+        )
+
+    def get_affected_spaces(
+        self, board_copy: List[List[SudokuSpace]], x: int, y: int
+    ) -> Set[SudokuSpace]:
+        """
+        Return the spaces in the same row, column, and square of the given space.
+
+        Args:
+            board_copy (List[List[SudokuSpace]]): A copy of the current board.
+            x (int): The x-coordinate of a space.
+            y (int): The y-coordinate of a space.
+
+        Raises:
+            ValueError: Either coordinate is out of range
+
+        Returns:
+            A list of spaces that can be affected by the given coordinates.
+        """
+        if (x < 0 or x >= self.board_size) or (y < 0 or y >= self.board_size):
+            raise IndexError("One or more of the given coordinates are out of range")
+
+        affected_spaces = set(
+            [board_copy[y][other_coord] for other_coord in range(self.board_size)]
+            + [board_copy[other_coord][x] for other_coord in range(self.board_size)]
+        )
+
+        square_size: int = int(self.board_size**0.5)
+        top_left_x = x - x % square_size
+        top_left_y = y - y % square_size
+
+        for y_offset in range(square_size):
+            for x_offset in range(square_size):
+                affected_spaces.add(
+                    board_copy[top_left_y + y_offset][top_left_x + x_offset]
+                )
+
+        affected_spaces.remove(board_copy[y][x])
+        return affected_spaces
+
+    def propegate(self, selected_tile: SudokuSpace) -> Self | None:
+        """
+        Propegate a given change throughout the entire board.
+
+        Arg:
+            selected_tile (SudokuSpace): The selected space to collapse the wavefunction under.
+
+        Returns:
+            The updated wavefunction if all spaces still have valid options. None otherwise.
+        """
+        current_board_copy: List[List[SudokuSpace]] = [
+            [space.copy() for space in row] for row in self.wavefunction
+        ]
+
+        current_board_copy[selected_tile.y][selected_tile.x] = selected_tile.copy()
+        stack: List[SudokuSpace] = [selected_tile]
+        while len(stack) > 0:
+            current_space = stack.pop()
+            space: SudokuSpace
+            for space in self.get_affected_spaces(
+                current_board_copy, current_space.x, current_space.y
+            ):
+                if space.value is not None:
+                    continue
+                space_possible_values = space.possible_values
+                current_space_neighbors = current_space.neighbors
+
+                new_options = space_possible_values & current_space_neighbors
+                if new_options and space.possible_values != new_options:
+                    space.possible_values = new_options
+
+                    if not space in stack:
+                        stack.append(space)
+                elif not new_options:
+                    return None
+        new_board = SudokuBoard(self.board_size)
+
+        new_board._wavefunction = current_board_copy
+        return new_board
+
+    def __str__(self) -> str:
+        """
+        Visualizes the current state of the wavefunction.
+
+        Returns:
+            A string representation of the wavefunction
+        """
+        output: str = "--------" * self.board_size + "\n|"
+        for row in self.wavefunction:
+            for space in row:
+                output = (
+                    output
+                    + (str(space.value) if space.value is not None else "None")
+                    + "\t|"
+                )
+            output = output + "\n" + "|-------" * self.board_size + "|\n|"
+        output = "\n".join(output.split("\n")[:-2])
+        output = output + "\n" + "--------" * self.board_size
+        return output[:-1]
---
+++ src/wfc/sudoku_wfc/sudoku_space.py
@@ -0,0 +1,102 @@
+"""This module implements a space in a sodoku board used in the WFC algorithm."""
+import random
+from typing import Self
+from ..wfc_tile import WFCTile
+
+class SudokuSpace(WFCTile[int]):
+    """
+    An immutable type that represents a space on a sudoku space.
+
+    Args:
+        x (int): the x-coordinate of the space
+        y (int): the y-coordinate of the space
+        max_value (int): the maximum value of this space
+        value (int | None): a starting value between 1 and 9 inclusive. Defaults to None.
+
+    Attributes:
+        x (int): the x-coordinate of the space
+        y (int): the y-coordinate of the space
+        max_value (int): the maximum value of this space
+        value (T | None): The value represented by this tile.
+        possible_values (Set[T]): All possible values for this tile.
+        neighbors (Set[T]): A set of all possible neighbors, initialized to contain every
+        option.
+    """
+
+    def __init__(
+        self,
+        x: int,
+        y: int,
+        max_value: int,
+        value: int | None = None,
+    ) -> None:
+        """
+        Initilizes new sudoku space.
+
+        Args:
+            x (int): the x-coordinate of the space
+            y (int): the y-coordinate of the space
+            max_value (int): the maximum value of this space
+            value (int | None): a starting value between 1 and 9 inclusive. Defaults to None.
+
+        Raises:
+            ValueError: value is not between 1 and the max_value.
+        """
+        if value and (value < 1 or value > max_value):
+            raise ValueError(
+                f"Value is {value}, but must be between 1 and {max_value} inclusive."
+            )
+
+        super().__init__(x, y, value)
+        self.max_value = max_value
+        self.neighbors = set(range(1, max_value + 1))
+        self.possible_values = set(range(1, max_value + 1)) if not value else {value}
+        if value:
+            self.neighbors.remove(value)
+
+    def validate_value(self, new_value: int) -> None:
+        """
+        Check that a new_value is valid.
+
+        Raises:
+            ValueError: New value is outside of the allowed range.
+        """
+        if new_value and (new_value < 1 or new_value > self.max_value):
+            raise ValueError(
+                f"Value is {new_value}, but must be between 1 and {self.max_value} inclusive."
+            )
+
+    def collapase(self) -> Self | None:
+        """
+        Collapses the space to a single randomly-selected valid value.
+
+        Returns:
+            A new WFCTile if there are still valid options. None otherwise.
+        """
+        if self.possible_values is set():
+            return None
+
+        updated_space = SudokuSpace(self.x, self.y, self.max_value)
+        updated_space.value = random.choice(tuple(self.possible_values))
+        updated_space.possible_values = (
+            set(value for value in self.possible_values if value != updated_space.value)
+            if len(self.possible_values) > 1
+            else {updated_space.value}
+        )
+        updated_space.neighbors = set(
+            value for value in self.neighbors if value != updated_space.value
+        )
+        return updated_space
+
+    def copy(self) -> Self:
+        """
+        Create a copy of the space.
+
+        Returns:
+            A copy of the space.
+        """
+        new_copy = SudokuSpace(self.x, self.y, self.max_value, self.value)
+        new_copy.possible_values = self.possible_values.copy()
+        new_copy.neighbors = self.neighbors.copy()
+
+        return new_copy
